14e3ec8b8966f3e1a834a5a709f8ab2e32fbf726
David Dugger <ddugger@techtonic.com>
2018-10-18 15:21:49 -0600

    sliding divs working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    show: ""
+  };
+
+  showDiv = divToShow => {
+    this.setState({
+      show: divToShow
+    });
+  };
+
   render() {
+    const { show } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="wrapper">
+        <div className="one" className="button-wrapper">
+          <button onClick={() => this.showDiv("first")} className="one">
+            1
+          </button>
+          <button onClick={() => this.showDiv("second")} className="one">
+            2
+          </button>
+          <button onClick={() => this.showDiv("third")} className="one">
+            3
+          </button>
+        </div>
+        <div className="one" className="content-wrapper">
+          <div className={`one ${show === "first" ? "show" : "hide"}`}>
+            Content 1
+          </div>
+          <div className={`one ${show === "second" ? "show" : "hide"}`}>
+            Content 2
+          </div>
+          <div className={`one ${show === "third" ? "show" : "hide"}`}>
+            Content 3
+          </div>
+        </div>
       </div>
     );
   }
